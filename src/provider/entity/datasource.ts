637400a62a1a30bb4b9bbfb3f9a5fad297dc270b
caoaolong <eason105cc@163.com>
2025-12-15 10:56:53 +0800

    feat: update SVG icons and enhance datasource functionality
    - Replaced the edit-light.svg icon with a new design.
    - Added new SVG icons for Column_dark.svg, Column_light.svg, Index_dark.svg, Index_light.svg, plus-dark.svg, and plus-light.svg.
    - Removed obsolete SVG icons: add.svg and logo.svg.
    - Enhanced the Datasource class to support new types: fieldType and indexType.
    - Implemented methods to list objects and indexes in the datasource.
    - Updated SQL queries to correctly reference parent schema and table names.
    - Adjusted icon paths for field and index types in the Datasource class.

--- src/provider/entity/datasource.ts
+++ src/provider/entity/datasource.ts
@@ -5,7 +5,14 @@ import path from "path";
 const iconDir: string[] = ["..", "..", "resources", "icons"];
 
 export interface DatasourceInputData {
-  type: "datasource" | "collection" | "document" | "field" | "index";
+  type:
+    | "datasource"
+    | "collection"
+    | "document"
+    | "field"
+    | "index"
+    | "fieldType"
+    | "indexType";
 
   name: string;
   tooltip: string;
@@ -54,15 +61,111 @@ export class Datasource extends vscode.TreeItem {
       case "collection":
         return this.listTables();
       case "document":
+        return this.listObjects();
+      case "fieldType":
         return this.listColumns();
+      case "indexType":
+        return this.listIndexes();
       default:
         return Promise.resolve([]);
     }
   };
 
+  private listObjects(): Promise<Datasource[]> {
+    return new Promise<Datasource[]>((resolve) => {
+      resolve([
+        new Datasource(
+          {
+            type: "fieldType",
+            name: "字段",
+            tooltip: "",
+          },
+          this.root,
+          this
+        ),
+        new Datasource(
+          {
+            type: "indexType",
+            name: "索引",
+            tooltip: "",
+          },
+          this.root,
+          this
+        ),
+      ]);
+    });
+  }
+
+  private listIndexes(): Promise<Datasource[]> {
+    return new Promise<Datasource[]>((resolve) => {
+      if (
+        !this.root ||
+        !this.root.conn ||
+        !this.parent ||
+        !this.parent.parent
+      ) {
+        return resolve([]);
+      }
+      const conn = this.root.conn;
+      conn.query(
+        `
+SELECT 
+	INDEX_NAME AS iname,
+	COLUMN_NAME AS cname,
+	SEQ_IN_INDEX AS sii,
+	NON_UNIQUE AS nu,
+	INDEX_TYPE AS it
+FROM 
+    information_schema.STATISTICS 
+WHERE 
+    TABLE_SCHEMA = '${this.parent.parent.label}'
+    AND TABLE_NAME = '${this.parent.label}'
+ORDER BY 
+    INDEX_NAME, SEQ_IN_INDEX;
+`,
+        (err, results) => {
+          if (err) {
+            vscode.window.showErrorMessage(`查询索引失败：${err.message}`);
+            return resolve([]);
+          }
+          const indexes = new Map<string, string[]>();
+          const rows = results as any[];
+          for (const row of rows) {
+            if (!indexes.has(row["iname"] as string)) {
+              indexes.set(row["iname"] as string, [
+                `${row["it"]}`,
+                `${row["nu"]}`,
+              ]);
+            }
+            indexes.get(row["iname"] as string)?.push(`${row["cname"]}`);
+          }
+          const result: Datasource[] = [];
+          for (const [k, v] of indexes) {
+            const indexNames = v.slice(2).join(", ");
+            const tooltip = `${v[0]}(${indexNames})`;
+            result.push(
+              new Datasource({
+                name: k,
+                tooltip: tooltip,
+                extra: parseInt(v[1]) === 0 ? `UNIQUE` : ``,
+                type: "index",
+              })
+            );
+          }
+          return resolve(result);
+        }
+      );
+    });
+  }
+
   private listColumns(): Promise<Datasource[]> {
     return new Promise<Datasource[]>((resolve) => {
-      if (!this.root || !this.root.conn || !this.parent) {
+      if (
+        !this.root ||
+        !this.root.conn ||
+        !this.parent ||
+        !this.parent.parent
+      ) {
         return resolve([]);
       }
       const conn = this.root.conn;
@@ -75,8 +178,8 @@ SELECT
 FROM 
     information_schema.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = '${this.parent.label}'
-    AND TABLE_NAME = '${this.label}'
+    TABLE_SCHEMA = '${this.parent.parent.label}'
+    AND TABLE_NAME = '${this.parent.label}'
 ORDER BY 
     ORDINAL_POSITION;
 `,
@@ -85,7 +188,6 @@ ORDER BY
             vscode.window.showErrorMessage(`查询数据库失败：${err.message}`);
             return resolve([]);
           }
-          console.log(results);
           return resolve(
             (results as any[]).map(
               (row) =>
@@ -94,7 +196,7 @@ ORDER BY
                     name: row["name"] as string,
                     tooltip: row["cc"] as string,
                     extra: row["ctype"] as string,
-                    type: "document",
+                    type: "field",
                   },
                   this.root
                 )
@@ -187,7 +289,7 @@ ORDER BY
     this.root = root;
     this.parent = parent;
     this.type = input.type;
-		this.tooltip = input.tooltip;
+    this.tooltip = input.tooltip;
     this.contextValue = "dsItem";
     // 设置节点的可折叠状态：如果是 datasource（可展开以列出数据库），则设置为 Collapsed
     if (input.type === "field" || input.type === "index") {
@@ -205,7 +307,43 @@ ORDER BY
       case "document":
         this.initDocument(input);
         break;
+      case "field":
+      case "fieldType":
+        this.initFieldType(input);
+        break;
+      case "index":
+      case "indexType":
+        this.initIndexType(input);
+        break;
     }
+  }
+
+  private initIndexType(input: DatasourceInputData): void {
+    if (input.type === "index") {
+      this.description = input.extra;
+    }
+    this.iconPath = {
+      light: vscode.Uri.file(
+        path.join(__filename, ...iconDir, "Index_light.svg")
+      ),
+      dark: vscode.Uri.file(
+        path.join(__filename, ...iconDir, "Index_dark.svg")
+      ),
+    };
+  }
+
+  private initFieldType(input: DatasourceInputData): void {
+    if (input.type === "field") {
+      this.description = input.extra;
+    }
+    this.iconPath = {
+      light: vscode.Uri.file(
+        path.join(__filename, ...iconDir, "Column_light.svg")
+      ),
+      dark: vscode.Uri.file(
+        path.join(__filename, ...iconDir, "Column_dark.svg")
+      ),
+    };
   }
 
   private initDocument(input: DatasourceInputData): void {
